5283231841e2e6ae709078530def6d9e8a0dd477
Taras Garan'ko <garanko.t@gmail.com>
2021-11-17 17:48:22 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+const body = document.querySelector('body')
+let timerId = null
+
+startBtn.addEventListener('click', onChangeBodyColor)
+
+stopBtn.addEventListener('click', onStop)
+
+function onChangeBodyColor() {
+    timerId = setInterval(() => {
+    createBodyColor()
+    }, 1000);
+    startBtn.setAttribute("disabled", true)
+}
+
+function createBodyColor() {
+    const bodyColor = getRandomHexColor();
+    bodyRef.style.backgroundColor = bodyColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStop() {
+    clearInterval(timerId);
+    startBtnRef.removeAttribute("disabled");
+}
