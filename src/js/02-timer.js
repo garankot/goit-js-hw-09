47e38bd75fd8480c035c0481e51864e607c5135a
Taras Garan'ko <garanko.t@gmail.com>
2021-11-18 17:51:21 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,26 @@
 import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+
+let timerInput = document.getElementById("datetime-picker");
+let buttonRun = document.getElementById("[data-start]")
+let timerShow = document.getElementById(".timer");
+
+const timer = setInterval(function () {
+    seconds = timeMinut%60 // Получаем секунды
+    minutes = timeMinut/60%60 // Получаем минуты
+    hour = timeMinut/60/60%60 // Получаем часы
+    // Условие если время закончилось то...
+    if (timeMinut <= 0) {
+        // Таймер удаляется
+        clearInterval(timer);
+        // Выводит сообщение что время закончилось
+        alert("Время закончилось");
+    } else { // Иначе
+        // Создаём строку с выводом времени
+        let strTimer = `${Math.trunc(hour)}:${Math.trunc(minuts)}:${seconds}`;
+        // Выводим строку в блок для показа таймера
+        timerShow.innerHTML = strTimer;
+    }
+    --timeMinut; // Уменьшаем таймер
+}, 1000)
