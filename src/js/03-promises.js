1b36c1b1722600c16973598da0bfd8958ad9d418
Taras Garan'ko <garanko.t@gmail.com>
2021-11-23 14:49:18 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,20 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const el = {
+  form: document.querySelector('form'),
+  delay: document.querySelector('[name=delay]'),
+  step: document.querySelector('[name=step]'),
+  amount: document.querySelector('[name=amount]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    resolve({ position, delay });
   } else {
     // Reject
+    reject({ position, delay });
   }
 }
+console.log(createPromise);
+createPromise(1000, 2);
